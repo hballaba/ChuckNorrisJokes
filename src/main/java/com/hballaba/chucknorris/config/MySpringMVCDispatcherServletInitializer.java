4308add8846babb69ae185659a3de1872340fee7
hballaba <ivskuznetsov@gmail.com>
2021-10-09 13:47:08 +0300

    Configuration project, include thymeleaf

---
+++ src/main/java/com/hballaba/chucknorris/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.hballaba.chucknorris.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//Setting tomcat
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+//        указываем класс конфигурации
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+//        указываем корень dispatcherServlet
+        return new String[] {"/"};
+    }
+}
