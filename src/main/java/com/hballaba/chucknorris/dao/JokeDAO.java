515d58b5d2e44ff2590ab364db5cf810ababeaa0
hballaba <ivskuznetsov@gmail.com>
2021-10-14 11:52:39 +0300

    add and setting log4j and lombok

--- src/main/java/com/hballaba/chucknorris/dao/JokeDAO.java
+++ src/main/java/com/hballaba/chucknorris/dao/JokeDAO.java
@@ -1,6 +1,9 @@
 package com.hballaba.chucknorris.dao;
 
 import com.hballaba.chucknorris.models.Joke;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,13 +15,19 @@ public class JokeDAO {
 
     private JdbcTemplate jdbcTemplate;
 
+
     @Autowired
     public JokeDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Joke> index() {
-        List<Joke> jokes = jdbcTemplate.query("SELECT * FROM jokes", new JokeMapper());
+        Logger logger = LogManager.getLogger(JokeDAO.class);
+        String query = "SELECT * FROM jokes";
+
+        logger.info(query);
+
+        List<Joke> jokes = jdbcTemplate.query(query, new JokeMapper());
         for(Joke joke : jokes)
             System.out.println("here " + joke);
         return jokes;
